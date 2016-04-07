78d054e6a7eddd81c5b57708b6a4264612f7df80
ukatama <dev.ukatama@gmail.com>
2016-04-07 21:41:56 +0900

    Image table

--- src/server/knex.js
+++ src/server/knex.js
@@ -6,27 +6,58 @@ export const knex = Knex(config.get('database'));
 export const initTable = () => {
     if (!knex) return;
 
-    return knex.schema.hasTable('characters')
-        .then((exists) => {
-            if (exists) return;
+    const tables = {
+        characters: (table) => {
+            table.string('id').primary();
+            table.string('user_id').notNullable();
+            table.string('name').notNullable();
+            table.string('type').notNullable();
+            table.json('data').nullable();
+            table
+                .timestamp('created')
+                .notNullable()
+                .default(knex.fn.now());
+            table
+                .timestamp('modified')
+                .notNullable()
+                .default(knex.fn.now());
+            table.timestamp('deleted').nullable().default(null);
+        },
+        images: (table) => {
+            table.string('id').primary();
+            table.string('user_id').notNullable();
+            table.string('type').notNullable();
 
-            return knex.schema.createTable('characters', (table) => {
-                table.string('id').primary();
-                table.string('user_id').notNullable();
-                table.string('name').notNullable();
-                table.string('type').notNullable();
-                table.json('data').nullable();
-                table
-                    .timestamp('created')
-                    .notNullable()
-                    .default(knex.fn.now());
-                table
-                    .timestamp('modified')
-                    .notNullable()
-                    .default(knex.fn.now());
-                table.timestamp('deleted').nullable().default(null);
-            });
-        })
-        .then(() => {});
+            const client = config.get('database.client');
+            if (client.match(/^mysql2?$/)) {
+                table.specificType('data', 'MEDIUMBLOB');
+            } else {
+                table.binary('data');
+            }
+
+            table
+                .timestamp('created')
+                .notNullable()
+                .default(knex.fn.now());
+            table
+                .timestamp('modified')
+                .notNullable()
+                .default(knex.fn.now());
+            table.timestamp('deleted').nullable().default(null);
+        },
+    };
+
+    return Promise.all(
+        Object.keys(tables)
+            .map(
+                (name) => knex.schema
+                    .hasTable(name)
+                    .then(
+                        (exists) =>
+                            exists ||
+                                knex.schema.createTable(name, tables[name])
+                    )
+            )
+    ).then(() => {});
 };
 initTable();
