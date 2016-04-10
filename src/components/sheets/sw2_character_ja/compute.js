5b129562ff913feb363c146ecea46f6df1fee657
ukatama <dev.ukatama@gmail.com>
2016-04-10 18:54:14 +0900

    Correction

--- src/components/sheets/sw2_character_ja/compute.js
+++ src/components/sheets/sw2_character_ja/compute.js
@@ -32,6 +32,7 @@ const computeAbility = (data) => {
                 `ability_${keys[0]}`,
                 `ability_${keys[1]}`,
                 `growth_${keys[1]}`,
+                `correction_${keys[1]}`,
             ])
             .values()
             .filter()
@@ -43,7 +44,79 @@ const computeAbility = (data) => {
     });
 };
 
+const CorrectionKeys = [
+        'note',
+    ].concat([
+            'head',
+            'face',
+            'ear',
+            'neck',
+            'back',
+            'rhand',
+            'lhand',
+            'waist',
+            'leg',
+            'other',
+    ].map((key) => `ornament_${key}_effect`));
+const CorrectionTable = {
+    correction_a: ['dex', '器用度', '器用'],
+    correction_b: ['agi', '敏捷度', '敏捷'],
+    correction_c: ['str', '筋力'],
+    correction_d: ['vit', '生命力', '生命'],
+    correction_e: ['int', '知力'],
+    correction_f: ['spr', '精神力', '精神'],
+    speed: ['speed', 'spd', '移動力', '移動'],
+    limited_speed: ['制限移動'],
+    maximum_speed: ['全力移動'],
+    hp: ['HP'],
+    mp: ['MP'],
+    magic_power_correction: ['魔力'],
+    protection_correction: ['防護', '防護点'],
+    evasion_correction: ['回避', '回避力'],
+};
+const CorrectionMap = {};
+Object.keys(CorrectionTable)
+    .forEach(
+        (key) => CorrectionTable[key]
+            .forEach((alias) => (CorrectionMap[alias] = key))
+    );
+const CorrectionPattern = new RegExp(
+    `\\[(${Object.keys(CorrectionMap).join('|')})([+-=＋－＝])([0-9０-９]+)m?\\]`,
+    'gi'
+);
+const computeAbilityCorrections = (data) => {
+    CorrectionKeys
+        .map((key) => data[key])
+        .concat((data.feats || []).map((item) => item.effect))
+        .concat((data.weapons || []).map((item) => item.note))
+        .filter((value) => value)
+        .map((value) => value.replace(
+            CorrectionPattern,
+            // eslint-disable-next-line max-params
+            (all, name, op, num) => {
+                const key = CorrectionMap[name];
+                if (!key) return;
+
+                switch (op) {
+                    case '+':
+                    case '＋':
+                        data[key] = (+data[key] || 0) + (+num);
+                        break;
+                    case '-':
+                    case '－':
+                        data[key] = (+data[key] || 0) - (+num);
+                        break;
+                    case '=':
+                    case '＝':
+                        data[key] = +num;
+                        break;
+                }
+            }
+        ));
+};
+
 export const compute = (data) => {
+    computeAbilityCorrections(data);
     data.level = data.skills && _(data.skills)
         .map('level')
         .max();
@@ -68,7 +141,9 @@ export const compute = (data) => {
             const table = m[2];
 
             if (cat === '魔') {
-                skill.magic_power = +skill.level + +data.int_bonus;
+                skill.magic_power = (+skill.level) +
+                    (+data.int_bonus) +
+                    (+data.magic_power_correction || 0);
             }
 
             const exp = table === 'A' ? EXP_A : EXP_B;
@@ -99,8 +174,8 @@ export const compute = (data) => {
             .map('level')
             .max();
     data.initiative = scout_wl_level > 0 ? scout_wl_level + data.int_bonus : 0;
-    data.limited_speed = 3;
-    data.speed = data.agi;
+    data.limited_speed = data.limited_speed || 3;
+    data.speed = (+data.speed || 0) + data.agi;
     data.maximum_speed = data.speed * 3;
     const evade_skill = data.skills &&
         data.skills.find(({skill}) => skill && skill === data.evade_skill);
@@ -109,9 +184,11 @@ export const compute = (data) => {
             evade_skill && evade_skill.level > 0
                 ? +evade_skill.level + data.agi_bonus
                 : 0
-        ) + (+data.armor_evasion || 0) + (+data.shield_evasion || 0);
+        ) + (+data.armor_evasion || 0) + (+data.shield_evasion || 0) +
+        (+data.evasion_correction || 0);
     data.protection =
-        (+data.armor_protection || 0) + (+data.shield_protection || 0);
+        (+data.armor_protection || 0) + (+data.shield_protection || 0) +
+        (+data.protection_correction || 0);
 
     return data;
 };
