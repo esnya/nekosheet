0caf3727d45b9fc74bff5163b8b8671080ed47d5
ukatama <dev.ukatama@gmail.com>
2016-04-18 13:05:03 +0900

    Theme

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,22 @@
+import {
+    blue500,
+    orange500,
+} from 'material-ui/styles/colors';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from '../browser/store';
 import {Home} from '../containers/home';
 import {Character} from '../containers/character';
 import {Router} from './router';
+
+const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: blue500,
+        accent1Color: orange500,
+    },
+});
 
 const route = window.INITIAL_ROUTE;
 const routes = {
@@ -13,6 +26,8 @@ const routes = {
 
 export const App = () => (
     <Provider store={store}>
-        <Router {...route} routes={routes} />
+        <MuiThemeProvider muiTheme={muiTheme}>
+            <Router {...route} routes={routes} />
+        </MuiThemeProvider>
     </Provider>
 );
